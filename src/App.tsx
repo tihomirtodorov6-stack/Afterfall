0f27beed6f0d8e65862ac1f2275a7d69d02775b6
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-08-26 22:02:49 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+export default function App() {
+  return (
+    <main
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#111",
+        color: "#fff",
+        fontFamily: "Arial, sans-serif",
+        textAlign: "center"
+      }}
+    >
+      <div>
+        <h1>AFTERFALL</h1>
+
+        <p>
+          🧟 3D Zombie Strategy Game
+        </p>
+
+        <p>
+          Server 001
+        </p>
+
+        <p>
+          City Level 1
+        </p>
+      </div>
+    </main>
+  );
+}
