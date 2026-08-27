dd865a36ebc8aac8661f7f244cb9ad2ba853881d
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-08-27 21:53:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // нямаме вече React, затова махаме @vitejs/plugin-react
+  plugins: [react()]
 })
