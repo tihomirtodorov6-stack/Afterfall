b25d6c4e90a5f15e8626c50e7e7db1b7b162650a
tihomirtodorov6-stack <tihomir.todorov6@icloud.com>
2026-08-28 19:46:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-self.addEventListener('install', e => {e.waitUntil(caches.open('v1').then(c=>c.addAll(['/'])))});self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE = 'letishta-v2';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.json'])));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{if(k!==CACHE) return caches.delete(k)}))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      const clone=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,clone));
+      return res;
+    }).catch(()=>caches.match(e.request))
+  );
+});
